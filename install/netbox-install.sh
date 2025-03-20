5a71f2f5b47f8666dabb625587df7d26f6b8868e
oneil1838 <32617957+oneil1838@users.noreply.github.com>
2025-03-20 22:50:50 +0100

    Update netbox-install.sh

--- install/netbox-install.sh
+++ install/netbox-install.sh
@@ -51,7 +51,7 @@ msg_ok "Set up PostgreSQL"
 msg_info "Installing NetBox (Patience)"
 cd /opt
 TEST=$(curl -s https://github.com/netbox-community/netbox/tags | grep -m 1  /netbox-community/netbox/releases/tag/ )
-RELEASE=echo ${TEST:105:5}
+RELEASE=${TEST:105:5}
 wget -q "https://github.com/netbox-community/netbox/archive/refs/tags/v${RELEASE}.zip"
 unzip -q "v${RELEASE}.zip"
 mv /opt/netbox-${RELEASE}/ /opt/netbox
